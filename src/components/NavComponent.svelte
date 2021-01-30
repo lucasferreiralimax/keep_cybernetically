aedcb468833fa66a1d3ce1c9c8a63b5a91e3ba86
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-01-29 20:52:16 -0800

    adjstement nav button with other path svg and hover in links

--- src/components/NavComponent.svelte
+++ src/components/NavComponent.svelte
@@ -31,25 +31,29 @@
     if(active) {
       document.querySelector('#main').style.width = 'calc(100% - 220px)'
       document.querySelector('#main').style.transform = 'translateX(220px)'
+      document.querySelector('.nav-button svg').setAttribute('viewBox', `0 0 511.76 511.76`)
+      document.querySelector('.nav-button svg path').setAttribute('d', `M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048 c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251 l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251 c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165 c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0 c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z`)
     } else {
       document.querySelector('#main').removeAttribute('style')
+      document.querySelector('.nav-button svg').setAttribute('viewBox', `0 0 56 56`)
+      document.querySelector('.nav-button svg path').setAttribute('d', `M28 0C12.561 0 0 12.561 0 28s12.561 28 28 28 28-12.561 28-28S43.439 0 28 0zm12 41H16a2 2 0 010-4h24a2 2 0 010 4zm0-11H16a2 2 0 010-4h24a2 2 0 010 4zm0-11H16a2 2 0 010-4h24a2 2 0 010 4z`)
     }
 	}
 </script>
 
 <template lang='pug'>
-button.nav-button(on:click='{handleMenu}')
+button.nav-button(class:active='{active}' on:click='{handleMenu}')
   svg(xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56 56" width="35px" height="35px")
     path(d="M28 0C12.561 0 0 12.561 0 28s12.561 28 28 28 28-12.561 28-28S43.439 0 28 0zm12 41H16a2 2 0 010-4h24a2 2 0 010 4zm0-11H16a2 2 0 010-4h24a2 2 0 010 4zm0-11H16a2 2 0 010-4h24a2 2 0 010 4z")
 nav.nav(class:active='{active}')
   +each('navLinks as item')
     +if('item.target')
-      a.target#testes(href='{item.link}' target='{item.target}' noroute)
+      a.nav-link(href='{item.link}' target='{item.target}' noroute)
         +if('item.icon')
           +html('item.icon')
         .text {item.title}
     +if('!item.target')
-      a.route#testes(href='{item.link}' use:link)
+      a.nav-link(href='{item.link}' use:link)
         +if('item.icon')
           +html('item.icon')
         .text {item.title}
@@ -81,25 +85,31 @@ nav.nav(class:active='{active}')
   width 70px
   &.active
     width 200px
-    a
+    .nav-link
       justify-content start
+      &:hover
+        transform scale(1.1)
     .text
       margin-left 20px
       opacity 1
       transition 1s all
-  a
-    color #fff
-    text-decoration none
-    display flex
-    padding 10px 20px 10px
-    align-items center
-    border-bottom 1px solid #333
-    &:hover
-      background #666
-    .text
-      width 0
-      opacity 0
-      pointer-events none
-      transition .3s all
+
+.nav-link
+  color #fff
+  text-decoration none
+  display flex
+  padding 10px 20px 10px
+  align-items center
+  border-bottom 1px solid #333
+  transition .4s all
+  &:hover
+    background #666
+    transform scale(1.2)
+    border-radius 0 10px 10px 0
+  .text
+    width 0
+    opacity 0
+    pointer-events none
+    transition .3s all
 </style>
 
