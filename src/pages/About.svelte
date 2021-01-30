8ce80dfa8adf39eba91d4b8ed3d470b785b638a5
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-01-29 20:51:49 -0800

    rewrite for language pug

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,2 +1,6 @@
-<h1>About</h1>
-<p>Welcome to my website</p>
+<template lang="pug">
+  h1 About
+  p
+    | Simple example with&nbsp;
+    a(href="https://github.com/lucasferreiralimax/keep_cybernetically") Svelte
+</template>
