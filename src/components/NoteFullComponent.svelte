f4cd8921b042a71436bf27a82266bfcd3029aa9b
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-02-24 21:58:15 -0800

    new component NoteFullComponent

---
+++ src/components/NoteFullComponent.svelte
@@ -0,0 +1,160 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+  import { text_area_resize } from './autoresize_textarea.js'
+
+  const dispatch = createEventDispatcher();
+
+  export let note_full;
+
+  function remove(index) {
+    dispatch('remove', { index: index });
+    note_full.active = false
+  }
+
+  function changeNote(note_full) {
+    dispatch('changeNote', { note_full: note_full });
+  }
+
+  function clickOutside(node) {
+    const handleClick = event => {
+      if (note_full.active && node && !node.contains(event.target) && !event.defaultPrevented) {
+        node.dispatchEvent(
+          new CustomEvent('click_outside', node)
+        )
+      }
+    }
+
+    document.addEventListener('click', handleClick, true);
+
+    return {
+      destroy() {
+        document.removeEventListener('click', handleClick, true);
+      }
+    }
+  }
+
+  function handleHiddenFullNote () {
+    note_full.active = false
+  }
+</script>
+
+<template lang='pug'>
+section.note-full(class:active='{note_full.active}' use:clickOutside on:click_outside='{handleHiddenFullNote}')
+  input#title-content-edit(type='text' bind:value='{note_full.title}' on:input='{changeNote.bind(this, note_full)}' placeholder='Titulo')
+  textarea#text-content-edit(bind:value='{note_full.text}' on:input='{changeNote.bind(this, note_full)}' placeholder='Criar uma nota...' use:text_area_resize)
+  button.minimize(on:click='{handleHiddenFullNote}' type="button") _
+  button.btn.danger(type='button' on:click='{remove.bind(this, note_full.index) || handleHiddenFullNote}') Excluir
+.overlay-full(class:active='{note_full.active}')
+</template>
+
+<style lang="stylus">
+ #title-content-edit
+  background rgba(0,0,0,0)
+  border 0
+  box-sizing border-box
+  color #fff
+  font-size 1.5em
+  outline none
+  width 100%
+  &::placeholder
+    color #fff
+
+ #text-content-edit
+  background rgba(0,0,0,0)
+  border 0
+  color #fff
+  margin 15px 0 0
+  max-width 100%
+  min-width 100%
+  outline none
+  overflow hidden
+  position relative
+  resize none
+  &::placeholder
+    color #fff
+
+#title-content-edit
+  padding-left 10px
+  margin 5px 0
+
+#title-content-edit,
+#text-content-edit
+  color #000
+  &::placeholder
+    color #000
+
+#text-content-edit
+  min-width calc(100% - 20px)
+  max-height calc(100vh - 300px)
+  overflow-y auto
+  padding 0 10px
+  text-align left
+  &::-webkit-scrollbar-track
+    background #fff
+  &::-webkit-scrollbar-thumb
+    background-color rgba(0,0,0,.2)
+    border 3px solid #fff
+
+.note-full
+  background #fff
+  border-radius 6px
+  box-shadow 0 10px 20px rgba(0,0,0,.25)
+  left 50%
+  max-width 400px
+  opacity 0
+  padding-bottom 10px
+  pointer-events none
+  position fixed
+  right 0
+  top 80px
+  transform translate(-50%, 0)
+  transform-origin top left
+  transition .3s all
+  width calc(100% - 20px)
+  z-index 99
+  &.active
+    opacity 1
+    pointer-events all
+    &:hover
+      transform scale(1.05) translate(-50%, 0)
+      .minimize
+        opacity 1
+        pointer-events all
+  .btn.danger
+    margin 10px 10px 0 0
+    float right
+  .minimize
+    -webkit-tap-highlight-color transparent
+    background #ccc
+    border 0
+    border-radius 3px
+    color #fff
+    cursor pointer
+    height 20px
+    line-height 0
+    opacity 0
+    pointer-events none
+    position absolute
+    right 10px
+    top 10px
+    transition .3s all
+    width 20px
+    &:hover
+      background #aaa
+      color #fff
+.overlay-full
+  background #000
+  bottom 0
+  left 0
+  opacity 0
+  pointer-events none
+  position fixed
+  right 0
+  top 0
+  transition .5s all
+  z-index 2
+  &.active
+    opacity .7
+    pointer-events all
+</style>
+
