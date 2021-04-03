ac32b2edfd991110793f4056307fb54f41a97698
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-04-03 01:54:48 -0700

    addition store

---
+++ src/store.js
@@ -0,0 +1,7 @@
+import { writable } from 'svelte/store';
+import { poems } from './poems.js'
+
+let notes_local = localStorage.getItem('notes')
+
+export let notes_store = writable(notes_local ? JSON.parse(notes_local) : poems);
+export let note_full_store = writable({});
