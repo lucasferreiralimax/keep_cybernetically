22d0189dcef96e35c72a5bc8277377297ff9ed94
Lucas <lucasferreiralimax@gmail.com>
2021-09-25 13:17:01 -0300

    Add variable dark

--- src/store.js
+++ src/store.js
@@ -6,3 +6,4 @@ let notes_local = localStorage.getItem('notes')
 export let notes_store = writable(notes_local ? JSON.parse(notes_local) : poems);
 export let note_full_store = writable({});
 export let menu_store = writable(false);
+export let dark_store = writable(false);
